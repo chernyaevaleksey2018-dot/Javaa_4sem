b62a31e9fa8554af9e78fb3c74eae8e98703208e
Chernyaev Aleksey <chernyaevaleksey2018@gmail.com>
2026-06-03 23:20:20 +0300

    Create prac_1.3.1.java

---
+++ Консольный ввод – вывод/prac_1.3.1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+        scan.close();
+    }
+}
